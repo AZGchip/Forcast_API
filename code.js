444024f1d80afaf59232a8eb2bfebaa99864affe
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-03-17 17:35:02 -0400

    created function to collect all requests. started work on template

--- code.js
+++ code.js
@@ -1,34 +1,76 @@
 var searchbarValue;
 var apiKey = "d7883c353f25f6ca1e88dfb9577483a0";
-var apiUrl;
-var infodump;
+
+
 $("#search").on("click", function (event) {
     event.stopPropagation();
     searchbarValue = $("#searchbox").val();
     searchbarValue = searchbarValue.toString();
-    buildUrl(searchbarValue);
+    buildUrl(searchbarValue, null,null);
 })
-function buildUrl(val) {
-    //search by city name
-    if ($("#byName").is(':checked')) {
-        apiUrl = "http://api.openweathermap.org/data/2.5/weather?q=" + val + "&appid=" + apiKey;
+function buildUrl(val, val2, val3) {
+    let apiString;
+    //if second value is null 
+    if (val2 === null) {
+        //search by city name
+        var searchBy;
+        if ($("#byName").is(':checked')) {
+            searchBy = "q";
+        }
+        //search by zip
+        else {
+            searchBy = "zip";
+        }
+        apiString = "http://api.openweathermap.org/data/2.5/weather?" + searchBy + "=" + val + ",&appid=" + apiKey;
+        requestData(apiString, 0);
     }
-    //search by zip
-    else {
-        apiUrl = "http://api.openweathermap.org/data/2.5/weather?zip=" + val + ",&appid=" + apiKey;
-    }
-    requestData();
+    else{
+        apiString = "http://api.openweathermap.org/data/2.5/uvi?appid=" + apiKey + "&lat=" + val + "&lon=" + val2
+        requestData(apiString, 1)
+        apiString = "http://api.openweathermap.org/data/2.5/forecast?&lat=" + val + "&lon=" + val2 +"&appid=" + apiKey
+        requestData(apiString, 2)
+        
+   }
 }
-function requestData() {
+function requestData(apiUrl, x) {
+            let infodump;
+            let uvData;
+            let forcast;
     $.ajax({
         url: apiUrl,
         method: "GET"
     })
         .then(function (response) {
-            infodump = response;
             
+            if (x === 0) {
+                infodump = response;
+                let lat = infodump.coord.lat;
+                let long = infodump.coord.lon;
+                buildUrl(lat, long, null)
+            }
+            else if(x === 1){
+                uvData = response;
+                
+            }
+            else{
+                forcast = response
+                getinfo(infodump,uvData,forcast)
+            }
 
-            
 
         });
+}
+function getinfo(current,uv,fiveDay){
+    infodump.weather[0].main
+    infodump.weather[0].description
+    infodump.main.temp
+    infodump.main.feels_like
+    infodump.main.temp_max
+    infodump.main.temp_min
+    infodump.main.humidity
+}
+function currentWeather(){
+    $("#current").html(`
+    
+    `)
 }
