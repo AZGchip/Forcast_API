44c5b6830e9ed1534290ad15f58db0eb31c636ff
AZGchip <60306563+AZGchip@users.noreply.github.com>
2020-03-19 17:46:23 -0400

    added function to list 5 day forcast

--- code.js
+++ code.js
@@ -2,6 +2,9 @@ var searchbarValue;
 var apiKey = "d7883c353f25f6ca1e88dfb9577483a0";
 var infodump;
 var metric = 0;
+var uvData;
+var forcast;
+var forcastSelect = [0, 13, 21, 29, 37]
 
 $("#search").on("click", function (event) {
     event.stopPropagation();
@@ -35,13 +38,13 @@ function buildUrl(val, val2, val3) {
 }
 function requestData(apiUrl, x) {
 
-    let uvData;
-    let forcast;
+
     $.ajax({
         url: apiUrl,
         method: "GET"
     })
         .then(function (response) {
+
 
             if (x === 0) {
                 infodump = response;
@@ -53,15 +56,17 @@ function requestData(apiUrl, x) {
                 uvData = response;
 
             }
-            else {
+            else if (x === 2) {
                 forcast = response
                 getinfo(infodump, uvData, forcast)
+
             }
 
 
         });
 }
-function getinfo(current, uv, fiveDay) {
+function getinfo(current, uv, forcast) {
+
     let cityName = current.name
     let weather = current.weather[0].main
     let weatherDesc = current.weather[0].description
@@ -73,19 +78,20 @@ function getinfo(current, uv, fiveDay) {
     let humidity = Math.round(infodump.main.humidity) + "%"
     let wind = current.wind;
     if (metric) {
-        temp = toCelsius(infodump.main.temp) 
-        feels = toCelsius(infodump.main.feels_like)  
-        high = toCelsius(infodump.main.temp_max) 
-        low = toCelsius(infodump.main.temp_min) 
+        temp = toCelsius(infodump.main.temp)
+        feels = toCelsius(infodump.main.feels_like)
+        high = toCelsius(infodump.main.temp_max)
+        low = toCelsius(infodump.main.temp_min)
         unit = "C";
     }
     else {
-        temp = toFahrenheit(infodump.main.temp) ;
-        feels = toFahrenheit(infodump.main.feels_like) 
-        high = toFahrenheit(infodump.main.temp_max) 
-        low = toFahrenheit(infodump.main.temp_min) 
+        temp = toFahrenheit(infodump.main.temp);
+        feels = toFahrenheit(infodump.main.feels_like)
+        high = toFahrenheit(infodump.main.temp_max)
+        low = toFahrenheit(infodump.main.temp_min)
         unit = "F";
     }
+    let uvVal = uv.value;
     $("#current").html(`
     <div class="row">
     <div class="col-12 text-center">tues march 17</div>
@@ -115,14 +121,16 @@ function getinfo(current, uv, fiveDay) {
 
 </div>
 <div class="row">
-    <h2 class="col-12 text-center">uv index</h2>w
+    <h2 class="col-12 text-center">uv index:${uvVal}</h2>
 </div>
 <div class="row" id="fiveForcast">
 
+
+<div class="col-sm-1"></div>
 </div>
 
     `)
-
+    buildForcast(forcast)
 }
 function toCelsius(k) {
     let celsius = Math.round(k - 273.15);
@@ -131,4 +139,58 @@ function toCelsius(k) {
 function toFahrenheit(k) {
     let fahrenheit = Math.round((k - 273.15) * 1.8 + 32);
     return fahrenheit;
+}
+function buildForcast(forc) {
+    forcastSelect.forEach(i => {
+
+
+
+        let date = forc.list[i].dt_txt;
+        let weather = forc.list[i].weather[0].main
+        let weatherDesc = forc.list[i].weather[0].description
+        let temp;
+        let unit;
+        let humidity = Math.round(forc.list[i].main.humidity) + "%"
+
+        if (metric) {
+            temp = toCelsius(forc.list[i].main.temp)
+
+            unit = "C";
+        }
+        else {
+            temp = toFahrenheit(forc.list[i].main.temp);
+            unit = "F";
+        }
+        let forcastDay = $(`<div class="col-2">`);
+        forcastDay.html(`
+    <div class="row">
+    <div class="col-12 text-center">${date}</div>
+</div>
+
+
+<div class="row-12 text-center">
+    <p>${weatherDesc}</p>
+</div>
+<div class="row-12 text-center">
+    <h3>${temp}&deg${unit}</h3>
+    <p class="text-center">Humidity: ${humidity}</p>
+</div>
+<div class="row">
+    
+    
+      
+       
+
+</div>
+
+
+
+</div>
+    `)
+        $("#fiveForcast").append(forcastDay)
+
+
+
+    })
+    
 }
